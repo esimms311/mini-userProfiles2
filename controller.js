286a9f4216b7923aec571e878e83a01e4922f0b7
esimms311 <eric.simmons26@gmail.com>
2016-11-29 14:14:27 -0700

    objective complete

--- controller.js
+++ controller.js
@@ -1,10 +1,29 @@
-angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+angular.module('userProfiles').controller('MainController', function ($scope, mainService) {
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  $scope.currentPage = 1;
+
+  $scope.getUsers = function () {
+    var promise = mainService.getUsers($scope.currentPage);
+
+    promise.then(function (response) {
+      $scope.users = response.data.data;
+      $scope.totalPages = response.data.total_pages;
+    })
   }
 
-
   $scope.getUsers();
+
+  $scope.prev = function () {
+    if ($scope.currentPage > 1) {
+      $scope.currentPage--;
+      $scope.getUsers();
+    }
+  }
+  $scope.next = function () {
+    if ($scope.currentPage < $scope.totalPages) {
+      $scope.currentPage++;
+      $scope.getUsers();
+    }
+  }
 
 });
